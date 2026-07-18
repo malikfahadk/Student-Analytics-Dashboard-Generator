7a445fa9a30cd0434340e3dae01986b56904f5f9
Muhammad Fahad <fahadkhokar786khokar@gmail.com>
2026-07-19 00:42:55 +0500

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Student-Analytics-Dashboard-Generator/',
   plugins: [react()],
 });
